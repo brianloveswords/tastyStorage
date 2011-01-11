fd1dfdfdafe1746a96ea1828e0260f3242cb7bba
Brian Brennan <brianloveswords@gmail.com>
2011-01-11 10:03:20 -0500

    Starting to organize file.

---
+++ tastystorage.js
@@ -0,0 +1,13 @@
+// step 1: figure out if localstorage is supported
+//   step 1a: if not, create cookie parser
+// step 2: create wrapper methods for setting localstorage properties
+// step 3?: get to work with sessionstore?
+
+
+var cookie = function(){
+  var SPOILED = ';expires=Tue, 13 Aug 1985 07:30:00 UTC';
+  var encode = function(object){};
+  var decode = function(string){};
+  var retrieve = function(){};
+  var store = function(){};
+};
