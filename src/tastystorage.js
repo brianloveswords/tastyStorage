bd01f337e1335dd300348849c0974bf49944b982
Brian Brennan <brianloveswords@gmail.com>
2011-01-11 15:54:09 -0500

    Change public interfaces for storage: mimic sessionStorage and localStorage instead of providing new public interface.

--- src/tastystorage.js
+++ src/tastystorage.js
@@ -1,14 +1,14 @@
 // determine JSON stringifier/parser to use.
 
 (function(global, document, JSON){
-  var prepare_cookie = function(){
+  var prepare_storage = function(scope){
     if (!JSON || !JSON.stringify || !JSON.parse) {
       throw new Error("tastystorage.js: Expecting a JSON object with stringify() and parse() methods.");
     }
     
-    var STORAGE_NAME = 'tastyStorage',
+    var STORAGE_NAME = scope+'Storage',
         STORAGE_REGEXP = RegExp(STORAGE_NAME+'=([a-zA-Z0-9%]*)'),
-        FRESH = ';expires=Tue, 13 Aug 2100 07:30:00 UTC',
+        FRESH = (scope == 'local') && ';expires=Tue, 13 Aug 2100 07:30:00 UTC' || '',
         SPOILED = ';expires=Tue, 13 Aug 1985 07:30:00 UTC',
         
         encode = function(object){
@@ -25,53 +25,49 @@
         })(),
         update = function(){
           document.cookie = STORAGE_NAME + '=' + encode(storage) + FRESH;
+        },
+        calculate_length = function(){
+          var len = 0;
+          for (var prop in storage) storage.hasOwnProperty(prop) && (len += 1);
+          return len;
         };
+    
     
     return {
       'interface': 'document.cookie',
       'clear': function() {
         document.cookie = STORAGE_NAME + '=null' + SPOILED;
+        this.length = 0;
       },
       'getItem': function(key) {
         return storage[key];
       },
       'setItem': function(key, value) {
         storage[key] = value;
+        this.length = calculate_length();
         update();
         return value;
       },
       'removeItem': function(key) {
         delete storage[key];
+        this.length = calculate_length();
         update();
       },
-      'length': function(){
-        var len = 0;
-        for (var prop in storage) storage.hasOwnProperty(prop) && (len += 1);
-        return len;
-      }
+      'length': calculate_length()
     };
   };
   
-  var prepare_local = function(){
-    return {
-      'interface': 'localStorage',
-      'clear': localStorage.clear,
-      'getItem': localStorage.getItem,
-      'setItem': localStorage.setItem,
-      'removeItem': localStorage.removeItem,
-      'length': function() {
-        return localStorage.length;
-      }
-    };
-  };
-
   /**
    * @constructor
    */
-  var StorageWrapper = function(){
-    var iface = global['localStorage'] && prepare_local() || prepare_cookie();
+  var StorageWrapper = function(scope){
+    var iface = prepare_storage(scope);
     for (var prop in iface) iface.hasOwnProperty(prop) && (this[prop] = iface[prop]);
   };
 
-  global['tastyStorage'] = new StorageWrapper();
+  (global['t_localStorage'] = new StorageWrapper('local'));
+  (global['t_sessionStorage'] = new StorageWrapper('session'));
+  
+  //global['localStorage'] || (global['t_localStorage'] = new StorageWrapper('local'));
+  //global['sessionStorage'] || (global['t_sessionStorage'] = new StorageWrapper('session'));
 })(this, this.document, JSON);
